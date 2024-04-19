641e17d62fc9e95ccfb452f86a1183c95d42b599
Aleksei-web-bit <2322929@mail.ru>
2024-04-19 11:38:56 +0300

    Методы авторизации и регистрации (система сообщает об ошибках)

--- bot/bot.cjs
+++ bot/bot.cjs
@@ -1,8 +1,40 @@
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf('7092969890:AAHukOvFRgIAYmOe2ZHMfv8N4WQdK-gXHp8');
+const bot = new Telegraf('YOUR_TELEGRAM_BOT_TOKEN');
+
+// Объект для хранения данных об авторизованных пользователях
+const authorizedUsers = {};
 
 bot.start((ctx) => ctx.reply('Привет! Я FitisovBot!'));
 
-bot.on('text', (ctx) => ctx.reply('Взаимно' + ctx.message.text));
+// Команда для авторизации с использованием ключа
+bot.command('auth', (ctx) => {
+    // Проверяем, отправил ли пользователь ключ вместе с командой
+    const key = ctx.message.text.split(' ')[1];
+    if (key === 'YOUR_AUTH_KEY') {
+        authorizedUsers[ctx.message.from.id] = true;
+        ctx.reply('Вы успешно авторизованы!');
+    } else {
+        ctx.reply('Неверный ключ авторизации.');
+    }
+});
+
+// Обработчик текстовых сообщений только для авторизованных пользователей
+bot.on('text', (ctx) => {
+    const userId = ctx.message.from.id;
+    if (authorizedUsers[userId]) {
+        ctx.reply('Взаимно ' + ctx.message.text);
+    } else {
+        ctx.reply('Пожалуйста, сначала авторизуйтесь с помощью /auth YOUR_AUTH_KEY');
+    }
+});
+
+// Команда для регистрации имени пользователя
+bot.command('register', (ctx) => {
+    const fullName = ctx.message.text.split(' ')[1];
+    // В этом примере просто сохраняем полное имя в объекте authorizedUsers, 
+    // но в реальном приложении вы, вероятно, захотите сохранить его в базе данных.
+    authorizedUsers[ctx.message.from.id] = { fullName };
+    ctx.reply('Спасибо за регистрацию, ' + fullName + '!');
+});
 
 bot.launch();
